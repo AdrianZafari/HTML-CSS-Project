256725a355b2a721b0b87ede938a0e3e0acffab1
AdrianZafari <adrian006@hotmail.com>
2024-09-20 16:56:45 +0200

    added subscribe section and CSS with theme swap support ato the HTML document, currently in decent state for submission

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 const toggleBtn = document.querySelector('.toggle_btn');
-        const toggleBtnIcon = document.querySelector('.toggle_btn i');
-        const dropdownMenu = document.querySelector('.dropdown_menu');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropdownMenu = document.querySelector('.dropdown_menu');
 
-        toggleBtn.onclick = function () {
-            dropdownMenu.classList.toggle('open')
-            const isOpen = dropdownMenu.classList.contains('open')
+toggleBtn.onclick = function () {
+    dropdownMenu.classList.toggle('open')
+    const isOpen = dropdownMenu.classList.contains('open')
 
-            //change icon depending on if menu is open
-            toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
-        };
+    //change icon depending on if menu is open
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
+};
 
 
-        const themeText = document.querySelector('.color-theme');
-        const sliderButton = document.querySelector('.switch input');
-        const body = document.body; // Reference to the body
-        
-        sliderButton.onclick = function() {
-            const isChecked = sliderButton.checked;
-    
-            if (isChecked) {
-                // Apply dark theme
-                document.body.classList.remove('light-theme');
-                document.body.classList.add('dark-theme');
-                themeText.innerHTML = "Dark theme"
-            } else {
-                // Apply light theme
-                document.body.classList.remove('dark-theme');
-                document.body.classList.add('light-theme');
-                themeText.innerHTML = "Light theme"
-            }
-        };
+const themeText = document.querySelector('.color-theme');
+const sliderButton = document.querySelector('.switch input');
+const body = document.body; // Reference to the body
 
-        var acc = document.getElementsByClassName("accordion-btn");//Code from W3 "How to create an Accordion"
-        var i;
+sliderButton.onclick = function() {
+    const isChecked = sliderButton.checked;
 
-        for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
-            if (panel.style.maxHeight) {
-                panel.style.maxHeight = null;
-            } else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-            } 
-        });
-        }
+    if (isChecked) {
+        // Apply dark theme
+        document.body.classList.remove('light-theme');
+        document.body.classList.add('dark-theme');
+        themeText.innerHTML = "Dark theme"
+    } else {
+        // Apply light theme
+        document.body.classList.remove('dark-theme');
+        document.body.classList.add('light-theme');
+        themeText.innerHTML = "Light theme"
+    }
+};
+
+var acc = document.getElementsByClassName("accordion-btn");//Code from W3 "How to create an Accordion"
+var i;
+
+for (i = 0; i < acc.length; i++) {
+acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+    } else {
+        panel.style.maxHeight = 30 + panel.scrollHeight + "px";
+    } 
+});
+}
